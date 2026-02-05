5a5db2638889b67e39732c44c2d0d090b98d5b10
Arinana <allyut12zk@gmail.com>
2026-02-05 18:06:10 +0300

    Изменения для деплоя

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -18,6 +18,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(int.Parse(port));
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
