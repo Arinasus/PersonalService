364f6cdfcd71d05c3e122e47bd971a727bee7ecf
Arinana <allyut12zk@gmail.com>
2026-02-05 18:15:43 +0300

    Изменение Program.cs

--- Program.cs
+++ Program.cs
@@ -4,20 +4,6 @@ using WebApplication2.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddSession();
-builder.Services.AddScoped<EmailService>();
-
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 
 builder.WebHost.ConfigureKestrel(options =>
@@ -25,12 +11,27 @@ builder.WebHost.ConfigureKestrel(options =>
     options.ListenAnyIP(int.Parse(port));
 });
 
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddSession();
+builder.Services.AddScoped<EmailService>();
+
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseSession();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
